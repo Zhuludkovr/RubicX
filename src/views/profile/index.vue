8418534b5a91acb7c5dffc74f2f66cc20787cd1c
tixdim <rzhuludkov@bk.ru>
2021-11-13 17:50:55 +0300

    server

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -3,16 +3,13 @@
     <div v-if="user">
       <el-row :gutter="20">
 
-        <el-col :span="24" :xs="24">
+        <el-col :span="8" :xs="24">
           <user-card :user="user" />
         </el-col>
 
-        <!-- <el-col :span="18" :xs="24">
+        <el-col :span="16" :xs="24">
           <el-card>
             <el-tabs v-model="activeTab">
-              <el-tab-pane label="Activity" name="activity">
-                <activity />
-              </el-tab-pane>
               <el-tab-pane label="Timeline" name="timeline">
                 <timeline />
               </el-tab-pane>
@@ -21,7 +18,7 @@
               </el-tab-pane>
             </el-tabs>
           </el-card>
-        </el-col> -->
+        </el-col>
 
       </el-row>
     </div>
@@ -31,17 +28,16 @@
 <script>
 import { mapGetters } from 'vuex'
 import UserCard from './components/UserCard'
-import Activity from './components/Activity'
 import Timeline from './components/Timeline'
 import Account from './components/Account'
 
 export default {
   name: 'Profile',
-  components: { UserCard, Activity, Timeline, Account },
+  components: { UserCard, Timeline, Account },
   data() {
     return {
       user: {},
-      activeTab: 'activity'
+      activeTab: 'timeline'
     }
   },
   computed: {
